bb05e57299079a4d56d8ce9ab76fff33532d237b
Fernando Colom <colomfernando@gmail.com>
2020-05-03 17:55:53 -0300

    add eslint config

--- src/utils.js
+++ src/utils.js
@@ -9,3 +9,13 @@
 
 export const validateNumber = (num) =>
 	!!(num || num === 0) && typeof num === 'number' && !Number.isNaN(Number(num));
+
+/**
+ * @name validateString
+ * @private
+ * @description return if arg is a valid string
+ * @param {string} string
+ * @example validateString('') // false
+ * @example validateString('fizzmod') // true
+ */
+export const validateString = (str = '') => !!(typeof str === 'string' && str);
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,28 +1,38 @@
-import { validateNumber } from '../src/utils';
+import { validateNumber, validateString } from '../src/utils';
 
 describe('utils::validateNumber', () => {
-	test('its should return false with string', () => {
+	test('should return false with string', () => {
 		const result = validateNumber('');
 		expect(result).toBe(false);
 	});
-	test('its should return false with string', () => {
+	test('should return false with string', () => {
 		const result = validateNumber('test');
 		expect(result).toBe(false);
 	});
-	test('its should return false with object', () => {
+	test('should return false with object', () => {
 		const result = validateNumber({});
 		expect(result).toBe(false);
 	});
-	test('its should return false with array', () => {
+	test('should return false with array', () => {
 		const result = validateNumber([]);
 		expect(result).toBe(false);
 	});
-	test('its should return false with NaN', () => {
+	test('should return false with NaN', () => {
 		const result = validateNumber(NaN);
 		expect(result).toBe(false);
 	});
-	test('its should return true', () => {
+	test('should return true', () => {
 		expect(validateNumber(4)).toBe(true);
 		expect(validateNumber(0)).toBe(true);
+	});
+});
+
+describe('utils::validateString', () => {
+	test('its should return false with empty arg', () => {
+		const result = validateString('');
+		expect(result).toBe(false);
+	});
+	test('should return true', () => {
+		expect(validateString('test')).toBe(true);
 	});
 });
