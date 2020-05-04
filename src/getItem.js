36c6ebe972573e8181204088788b519726031402
Fernando Colom <colomfernando@gmail.com>
2020-05-03 21:31:25 -0300

    add setItem func

---
+++ src/getItem.js
@@ -0,0 +1,26 @@
+import { validateString, isRequired, invalidType } from './utils';
+
+/**
+ * @name getItem
+ * @private
+ * @param {string} name name key for localstorage
+ * @description function for get data in localstorage
+ * @example setItem() // 		getItem('vtex-installments') // [{ visa: 2 }]
+ * @returns data from localstorage
+ */
+
+const getItem = (name = isRequired('name localstorage')) => {
+	const itemStr = localStorage.getItem(name);
+	if (!validateString(name)) invalidType('name', 'string');
+	if (!itemStr) {
+		return null;
+	}
+	const item = JSON.parse(itemStr);
+	if (item.expiry < new Date().getTime()) {
+		localStorage.removeItem(name);
+		return null;
+	}
+	return item.value;
+};
+
+export default getItem;
