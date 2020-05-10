3acb50489330ae140d8fc58073239c0d8281da85
Fernando Colom <colomfernando@gmail.com>
2020-05-10 20:58:28 -0300

    change directory

--- rollup.config.js
+++ rollup.config.js
@@ -19,7 +19,6 @@ export default [
 				sourcemap: true,
 			},
 		],
-		external: ['axios'],
 		plugins: [
 			external(),
 			babel({
@@ -28,10 +27,7 @@ export default [
 				comments: false,
 				exclude: ['node_modules/**'],
 				presets: ['@babel/preset-env'],
-				plugins: [
-					'@babel/plugin-proposal-object-rest-spread',
-					'@babel/plugin-transform-runtime',
-				],
+				plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime'],
 			}),
 			resolve(),
 			commonjs(),
